1fdee3d32c82738e6b2c867b494b45eea95207b1
imshantanu1994-alt <imshantanu1994@gmail.com>
2025-12-03 15:20:06 +0100

    Initial Commit

---
+++ Overthinking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Overthinking
+//
+//  Created by Shantanu Mishra  on 03/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Overthinking/OverthinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverthinkingApp.swift
+//  Overthinking
+//
+//  Created by Shantanu Mishra  on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OverthinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
